afdc938e931282c7acada6f058c5d1ff34929887
Helminth117 <Helminth117@users.noreply.github.com>
2026-06-18 16:57:07 -0600

    refact: modularizar ChatManager.jsx eliminando duplicidad de TTSFilter

--- src/renderer/overlay/components/ChatManager.jsx
+++ src/renderer/overlay/components/ChatManager.jsx
@@ -1,56 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { useOverlayStore } from '../../store';
 import { DraggableWidget } from './DraggableWidget';
-
-const TTSFilter = (() => {
-  const BLOCKED_WORDS = [
-    "pendejo","pinche","chingada","chingar","cabrón","cabron","puta","puto",
-    "culero","culo","verga","mamón","mamada","wey","güey","hijo de puta",
-    "gilipollas","imbécil","subnormal","idiota","coño","joder","mierda",
-    "marica","maricon","maricón","retrasado","mongolo","pelotudo","boludo",
-    "forro","perra","zorra","putona","ramera","bastardo","hdp","ctm",
-    "ojete","naco","mamon","chinga tu madre","vete a la chingada",
-    "me cago","hostia","capullo","estupido","tarado","baboso","menso",
-    "bruto","inútil","inutil","cagon","desgraciado","maldito",
-    "fuck","fucking","fucker","motherfucker","shit","bullshit","asshole",
-    "bitch","bastard","crap","dick","cock","pussy","cunt","whore","slut",
-    "nigga","nigger","faggot","fag","retard","moron","dumbass","jackass",
-    "dipshit","douchebag","prick","twat","wanker","tosser","bollocks",
-    "arse","arsehole","kys","kill yourself","scumbag","piece of shit",
-    "gemido","gimiendo","moan","moaning","orgasmo","orgasm","porno",
-    "hentai","rule34","sex","sexo","xxx","porn","nude","naked","horny",
-    "erotico","fetish","fetiche","onlyfans","only fans",
-    "te voy a matar","te voy a golpear","muerte","matar","kill","murder",
-    "bomb","bomba","shoot","apuñalar","gore","mutilación",
-    "suicidio","suicidate","suicide","autolesión","cortarse","self harm",
-    "self-harm","overdose","sobredosis",
-    "aaaa","aaaaaa","jajajajajaja","lolololol","xdxdxd",
-    "contraseña","password","dox","doxxing","doxear","hackeo",
-    "sé donde vives","i know where you live","datos personales",
-    "skibidi","chupapi","muñaño","mi bebito fiu fiu","gyat","rizz",
-    "fanum tax","looksmaxxing","brainrot","sigma male"
-  ];
-
-  const normalize = (text) => text.toLowerCase().replace(/\s+/g, ' ').replace(/[^\w\s]/g, '').replace(/(\b\w)\s(?=\w\b)/g, '$1');
-
-  const clean = (text) => {
-    if (!text) return '';
-    let result = text;
-    const normalized = normalize(text);
-
-    for (const word of BLOCKED_WORDS) {
-      const escaped = word.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-      const regex = new RegExp(`(?:^|\\W)${escaped}(?:\\W|$)`, 'gi');
-      if (regex.test(normalized)) {
-        const wordRegex = new RegExp(escaped, 'gi');
-        result = result.replace(wordRegex, (match) => '*'.repeat(match.length));
-      }
-    }
-    return result;
-  };
-
-  return { clean };
-})();
+import { TTSFilter } from '../utils/ttsFilter';
 
 export default function ChatManager() {
   const [messages, setMessages] = useState([]);
